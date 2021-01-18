aba8c4c6ab483730debbf51bf58ebc70d8908434
Lokesh Garia <63031525+lokeshgaria@users.noreply.github.com>
2021-01-18 21:44:08 +0530

    select to search
    select to search

--- imguploadajax/index.php
+++ imguploadajax/index.php
@@ -1,58 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+ <head>
+  <title>Webslesson Tutorial | How to Use Bootstrap Select Plugin with PHP JQuery</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+  <?php 
+  $conn = mysqli_connect("localhost","root","","youturn_genetics");
+   
+  $run = mysqli_query($conn ,  "select * from countries");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Upload</title>
-</head>
-
-<body>
-    <div class="main">
-        <div class="header">
-            <h1>upload & remove files <br> using ajax in php</h1>
-        </div>
-        <div id="content">
-            <form action="" id="submit-group">
-                <label for="">select image </label>
-                <input type="file" name="file" id="upload_file">
-                <span> Allowed file types : jpg , png , jpeg </span>
-
-            </form>
-            <br />
-            <div id="preview">
-                <div id="default">
-                <img src="download.png" alt="image">
-                </div>
-                <div id="image_preview">
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#upload_file').on("change", function() {
-
-                var fileData = new FormData($("#submit-group")[0]);
-                console.log(fileData);
-                $.ajax({
-                    url: "upload.php",
-                    type: "POST",
-                    data: fileData,
-                    contentType: false,
-                    processData: false,
-                    success: function(data) {
-                        $('#preview').show();
-                        $('#default').hide();
-                        $('#image_preview').html(data);
-                        $('#upload_file').val('');
-                    }
-                })
-            })
-        });
-    </script>
-</body>
-
+  ?>
+ </head>
+ <body>
+  <br /><br />
+  <div class="container">
+   <br />
+   <h2 align="center">How to Use Bootstrap Select Plugin with PHP JQuery</h2>
+   <br />
+   <div class="col-md-4" style="margin-left:200px;">
+    <form method="post" id="multiple_select_form">
+     <select name="framework" id="framework" class="form-control selectpicker" data-live-search="true"   ">
+      <option>-select country-</option> 
+     <?php 
+      while ($data = mysqli_fetch_assoc($run)) {  ?>
+           <option value="<?php echo $data['phonecode']; ?>"> <?php echo $data['name']?></option>
+     <?php } ?>
+     
+      
+     </select>
+     <br /><br />
+     <input type="hidden" name="hidden_framework" id="hidden_framework" />
+     
+    </form>
+    <br />
+    
+   </div>
+  </div>
+ </body>
 </html>
+
+<script>
+$(document).ready(function(){
+ $('.selectpicker').selectpicker();
+
+  
+
+  
+});
+</script>
